24f0cbf1722ac141f11f7b1277a5f0cb441a566a
Alex <alekshmykov@yandex.ru>
2021-11-14 12:53:54 +0300

    j

--- docker.sh
+++ docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+acc=cptmutchell
 if [[ "$1" =~ "flyway" ]]; then
   kubectl delete job "$1""-job" --ignore-not-found=true
-  DOCKER_BUILDKIT=1 docker build -t cptmutchell/"$1":"$2" -f "$3"/Dockerfile .
+  DOCKER_BUILDKIT=1 docker build -t ${acc}/"$1":"$2" -f "$3"/Dockerfile .
 
 else
-DOCKER_BUILDKIT=1 docker build -t cptmutchell/"$1":"$2" -f "$1"/Dockerfile .
+DOCKER_BUILDKIT=1 docker build -t ${acc}/"$1":"$2" -f "$1"/Dockerfile .
 fi
-docker push cptmutchell/"$1":"$2"
-docker rmi cptmutchell/"$1":"$2"
+docker push ${acc}/"$1":"$2"
+docker rmi ${acc}/"$1":"$2"
