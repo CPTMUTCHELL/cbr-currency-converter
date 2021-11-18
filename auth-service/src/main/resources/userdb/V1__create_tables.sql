49c1fd7d8feee26477c8328ff1e041fccb6a433b
Alex <alekshmykov@yandex.ru>
2021-11-18 17:41:29 +0300

    pg db creation

--- auth-service/src/main/resources/userdb/V1__create_tables.sql
+++ auth-service/src/main/resources/userdb/V1__create_tables.sql
@@ -1,5 +1,4 @@
 create database user_db;
-
 create table users(id serial primary key, username varchar,password varchar);
 create table roles(id serial primary key,name varchar);
 create table user_role(user_id int references users(id),role_id int references roles(id));
