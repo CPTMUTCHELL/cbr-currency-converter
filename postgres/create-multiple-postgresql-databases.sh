43f8ed892f6f1a4f3bc86ea922f59f2bcf4dad7d
Alex <alekshmykov@yandex.ru>
2021-12-10 15:28:00 +0300

    jenkinsfile

--- postgres/create-multiple-postgresql-databases.sh
+++ postgres/create-multiple-postgresql-databases.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#for db in "$@";
-#do
-echo "hf"
-psql -U postgres -h cbr-converter -tc "SELECT 1 FROM pg_database WHERE datname = 'olol'" | grep -q 1 | psql -U postgres -h cbr-converter -c "CREATE DATABASE olol"
-#done
+for db in "$@";
+do
+
+psql -U postgres -h cbr-converter -tc "SELECT 1 FROM pg_database WHERE datname = '$db'" \
+| grep -q 1 | psql -U postgres -h cbr-converter -c "CREATE DATABASE $db"
+done
