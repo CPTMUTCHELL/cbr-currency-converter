e777f644187924d58070b9b8cc0fba6f717671c6
Alex <alekshmykov@yandex.ru>
2021-12-10 19:06:57 +0300

    jenkinsfile

--- postgres-createdb/create-multiple-databases.sh
+++ postgres-createdb/create-multiple-databases.sh
@@ -3,5 +3,5 @@
 for db in "$@";
 do
 
-psql -U postgres -h cbr-converter -c "SELECT 'CREATE DATABASE $db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')"\gexec
+psql -U postgres -h cbr-converter -c "SELECT 'CREATE DATABASE $db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec"
 done
