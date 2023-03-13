808c6fa4845e36b4f2881cb13642c8b211e9e00d
Alex <cptmutchell@gmail.com>
2023-03-13 22:03:54 +0300

    ansible

--- jenkins/Readme.md
+++
@@ -1,2 +0,0 @@
-Example of jenkins creation inside Docker and it's connection to GCP.
-All envs were previously exported as environment variables on a linux machine.
--- jenkins/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gcloud auth activate-service-account jenkins-sa@aerobic-datum-327116.iam.gserviceaccount.com --key-file /usr/jenkins-sa.json --project=aerobic-datum-327116 && \
-    gcloud config set project aerobic-datum-327116 && \
-    gcloud container clusters get-credentials cluster-1 --zone europe-central2-a --project aerobic-datum-327116
-ansible-galaxy collection install kubernetes.core
-/sbin/tini -- /usr/local/bin/jenkins.sh
